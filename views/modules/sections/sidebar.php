1458accab9dba1c56453fdf8cbcb01cb1d311c90
OmarRios299 <161511271+ORO-esol@users.noreply.github.com>
2024-10-06 22:28:03 -0700

    OCCU-20

---
+++ views/modules/sections/sidebar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-plantilla" aria-label="Main navigation">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">OCCU</a>
+        <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link nav-item" aria-current="page" item='nav1' id='nav1' href="<?= $url . 'inicio'; ?>">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-item" aria-current="page" item='nav2' id='nav2' href="<?= $url . 'registrarme'; ?>">Registrarme</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle item-color nav-item" id='nav3' data-bs-toggle="dropdown" aria-expanded="false">Cafeterías</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item item-color nav-item" item='nav3' href="<?= $url . 'cafeterias_mapa'; ?>"><span>Ubicaciones</span></a></li>
+                        <li><a class="dropdown-item item-color nav-item" item='nav3' href="<?= $url . 'cafeterias_lista'; ?>"><span>Lista de cafeterías</span></a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <div class="row">
+                <div class="col-md-8 mt-2 ">
+                    <p class="letras-blancas">Hola, bienvenido<i class="bi bi-house"></i><span></span></p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-sesion" href="<?php echo $url ?>login"> Iniciar sesion </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</nav>
--- views/template.php
+++ views/template.php
@@ -2,206 +2,219 @@
 
 session_start();
 
-if(isset($_GET['action'])){
+if (isset($_GET['action'])) {
     $action = explode("/", $_GET['action']);
     $moduloActual = $action[0];
-}else{
+} else {
     $moduloActual = "dashboard";
 }
 
-if($moduloActual=="salir"){
-    setcookie('token_session', '', time(),'/'); 
+if ($moduloActual == "salir") {
+    setcookie('token_session', '', time(), '/');
     session_destroy();
     echo '<script>window.location="login"</script>';
 }
 
 //verificamos si hay session y una cookie del token
-if(!isset($_SESSION['iniciarSesion'])&&isset($_COOKIE['token_session'])){
+if (!isset($_SESSION['iniciarSesion']) && isset($_COOKIE['token_session'])) {
     //buscamos el usuario al que pertenece el usuario para hacer login
     LoginController::ingresoTokenController($_COOKIE['token_session']);
 }
 
 $template = new TemplateController();
-$url = $template -> obtenerUrlController();
+$url = $template->obtenerUrlController();
 $v = "1.0.0";
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>OCCU</title>
-        <meta name="theme-color" content="#000000">
-        <meta name="msapplication-navbutton-color" content="#000000">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-        <link rel="icon" sizes="192x192" href="<?php echo $url; ?>views/assets/css/img/favicon/favicon-192.png">
-    
-        <!-- Bootstrap CSS CDN -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        
-        <!-- DataTables -->
-        <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>views/assets/plugins/DataTables/datatables.min.css"/>
-        
-        <!-- Iconos -->
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        <!-- <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>views/assets/css/css/fontawesome/all.min.css"> -->
-        
-        <!-- Estilos internos -->
-        <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>views/assets/css/css/style.css">
-        
-        <!-- Scrollbar Custom CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>OCCU</title>
+    <meta name="theme-color" content="#000000">
+    <meta name="msapplication-navbutton-color" content="#000000">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-        <!-- Select2 stylesheet -->
-        <link href="<?php echo $url; ?>views/assets/plugins/select2/dist/css/select2.min.css" rel="stylesheet">
-        <!-- Select2 stylesheet -->
+    <link rel="icon" sizes="192x192" href="<?php echo $url; ?>views/assets/css/img/favicon/favicon-192.png">
 
-        <!-- Incluir Font Awesome (iconos) -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-        
-        
-        
-    </head>
-    <body id="body">
+    <!-- Bootstrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-        <input type="hidden" class="url" value="<?php echo $url; ?>">
+    <!-- DataTables -->
+    <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>views/assets/plugins/DataTables/datatables.min.css" />
 
-        <input type="hidden" class="moduloActual" value="<?php echo $moduloActual; ?>">
+    <!-- Iconos -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>views/assets/css/css/fontawesome/all.min.css"> -->
 
-        <?php
-        if(isset($_SESSION['iniciarSesion']) && $_SESSION['iniciarSesion'] == 'ok'){
+    <!-- Estilos internos -->
+    <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>views/assets/css/css/style.css">
 
-            //paginas sin sidebar ni navbar
-            if ($moduloActual == "cafeterias_mapa") {
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
-                echo '<div style="width: 100% !important; margin: 0 !important;height:100vh!important"><div class="">';
+    <!-- Select2 stylesheet -->
+    <link href="<?php echo $url; ?>views/assets/plugins/select2/dist/css/select2.min.css" rel="stylesheet">
+    <!-- Select2 stylesheet -->
+
+    <!-- Incluir Font Awesome (iconos) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+
+
+</head>
+
+<body id="body">
+
+    <input type="hidden" class="url" value="<?php echo $url; ?>">
+
+    <input type="hidden" class="moduloActual" value="<?php echo $moduloActual; ?>">
+
+    <?php
+    if (isset($_SESSION['iniciarSesion']) && $_SESSION['iniciarSesion'] == 'ok') {
+        // Determina si la página actual es "cafeterias_mapa" y estructura el layout en consecuencia
+        if ($moduloActual == "cafeterias_mapa") {
+            echo '<div style="width: 100%; margin: 0; height:100vh;"><div>';
+        } else {
+            echo '<div id="sistema">';
+            echo '<div class="contenido">';
+            include "modules/sections/navbar.php";
+            echo '<div class="modulos">';
+        }
+
+        // Determina el módulo a incluir basado en el valor de $action[0]
+        if (isset($action[0])) {
+            // Lista blanca de módulos permitidos sin importar el nivel
+            $modulosPermitidos = [
+                "dashboard",
+                "404",
+                "mantenimiento",
+                "cafeterias_lista",
+                "cafeterias_mapa",
+                "registrarme",
+                "salir"
+            ];
+
+            if (in_array($action[0], $modulosPermitidos)) {
+                echo '<div class="modulo-' . htmlspecialchars($action[0]) . '">';
+                include "modules/" . htmlspecialchars($action[0]) . ".php";
+                echo '</div>';
             } else {
-                //include "modules/sections/sidebar.php";
-                    echo '<div id="sistema">';
-                
-                echo '<div class="contenido">';
-                include "modules/sections/navbar.php";
-                echo '<div class="modulos">';
-            }
-                
-            //$_SESSION['nivel'] = 'Administrador';
-
-            if (isset($action[0])) {
-
-                //modulos que se incluyen sin importar el nivel
-                if (
-                    $action[0] == "dashboard"                   ||
-                    $action[0] == "404"                         ||
-                    $action[0] == "mantenimiento"               ||
-                    $action[0] == "cafeterias_lista"            ||
-                    $action[0] == "cafeterias_mapa"             ||
-                    $action[0] == "registrarme"                 ||
-                    $action[0] == "salir"
-                ) {
-    
-                    echo '<div class="modulo-' . $action[0] . '">';
-                    include "modules/" . $action[0] . ".php";
+                $modulo = GeneralController::buscarModuloSistema($action[0]);
+                if ($modulo && $modulo['permiso_modulo'] == 1) {
+                    echo '<div class="modulo-' . htmlspecialchars($action[0]) . '">';
+                    include "modules/" . htmlspecialchars($action[0]) . ".php";
                     echo '</div>';
                 } else {
-    
-                    //buscamos el modulo solicitado
-                    $modulo = GeneralController::buscarModuloSistema($action[0]);
-                    if ($modulo && ($modulo['permiso_modulo'] == 1)) {
-                        //si el usuario es nivel administrador tiene acceso a todos los niveles
-                        echo '<div class="modulo-' . $action[0] . '">';
-                        include "modules/" . $action[0] . ".php";
-                        echo '</div>';
-                    } else {
-                        //si el modulo no existe o si el nivel de usuario no tiene acceso al modulo 
-                        //incluimos 404
-                        echo '<div class="modulo-404">';
-                        include "modules/404.php";
-                        echo '</div>';
-                        
-                    }
+                    echo '<div class="modulo-404">';
+                    include "modules/404.php";
+                    echo '</div>';
                 }
-
-            }else{
-
-                echo '<div class="modulo-dashboard">';
-                include "modules/dashboard.php";
-                echo '</div>';
-
             }
-
-            echo '</div></div></div><div class="overlay"></div>';
-        }else if (isset($action[0])) {
-            if (
-                $action[0] == "404"                         ||
-                $action[0] == "mantenimiento"               ||
-                $action[0] == "cafeterias_lista"            ||
-                $action[0] == "cafeterias_mapa"             ||
-                $action[0] == "registrarme"                 
-            ) {
-                echo '<div class="modulo-' . $action[0] . '">';
-                include "modules/" . $action[0] . ".php";
-                echo '</div>';
-            } else {
-                echo '<div class="modulo-404">';
-                include "modules/404.php";
-                echo '</div>';
-            }
-        } else{
-
-            include "modules/login.php";
-
+        } else {
+            echo '<div class="modulo-dashboard">';
+            include "modules/dashboard.php";
+            echo '</div>';
         }
-        ?>
-                
 
-        <!-- jQuery     <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=geometry,drawing&callback=initMap"></script>
+        echo '</div></div></div><div class="overlay"></div>';
+    } else if (isset($action[0])) {
+        // Layout para las páginas públicas o sin sesión iniciada
+        if ($moduloActual == "cafeterias_mapa") {
+            echo '<div style="width: 100%; margin: 0; height:100vh;"><div>';
+        } else if ($moduloActual == "inicio") {
+            echo '<div id="pagina-inicial">';
+            echo '<div class="contenido">';
+            include "modules/sections/sidebar.php";
+            echo '<div>';
+        } else {
+            echo '<div id="sistema">';
+            echo '<div class="contenido">';
+            include "modules/sections/sidebar.php";
+            echo '<div class="modulos">';
+        }
+
+        // Lista blanca de módulos públicos permitidos
+        $modulosPublicos = [
+            "404",
+            "mantenimiento",
+            "cafeterias_lista",
+            "cafeterias_mapa",
+            "registrarme",
+            "inicio"
+        ];
+
+        if (in_array($action[0], $modulosPublicos)) {
+            echo '<div class="modulo-' . htmlspecialchars($action[0]) . '">';
+            include "modules/" . htmlspecialchars($action[0]) . ".php";
+            echo '</div>';
+        } else {
+            echo '<div class="modulo-404">';
+            include "modules/login.php";
+            echo '</div>';
+        }
+
+        echo '</div></div></div><div class="overlay"></div>';
+    } else {
+        echo '<div class="contenido">';
+        include "modules/sections/sidebar.php";
+        echo '<div class="modulos">';
+        include "modules/inicio.php";
+        echo '</div></div></div><div class="overlay"></div>';
+    }
+    ?>
+
+
+
+    <!-- jQuery     <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=geometry,drawing&callback=initMap"></script>
 -->
-        <script src="<?php echo $url; ?>views/assets/js/jquery-3.7.1.min.js"></script>
-        
-        <!-- Mapa google -->
-        <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAsDthuyGWYSIyMtMPjmbq7Epz8ABUrrHY&libraries=geometry,drawing&v=beta" defer></script>
+    <script src="<?php echo $url; ?>views/assets/js/jquery-3.7.1.min.js"></script>
 
-        <!-- Popper.js -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-        
-        <!-- Bootstrap JS -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        
-        <!-- SweetAlert -->
-        <script src="<?php echo $url; ?>views/assets/plugins/sweetalert/dist/sweetalert.min.js"></script>
-        
-        <!-- jQuery Custom Scroller CDN -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- Mapa google -->
+    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAsDthuyGWYSIyMtMPjmbq7Epz8ABUrrHY&libraries=geometry,drawing&v=beta" defer></script>
 
-        <!-- scripts -->
-        <script src="<?php echo $url; ?>views/assets/plugins/chartjs/chart.js"></script>
-        <script src="<?php echo $url; ?>views/assets/plugins/moment/min/moment.min.js"></script>
-        <script src="<?php echo $url; ?>views/assets/css/js/main.js"></script>
-        <script src="<?php echo $url; ?>views/assets/css/js/color-modes.js"></script>
-        <script src="<?php echo $url; ?>views/assets/plugins/select2/dist/js/select2.full.min.js"></script>
-        <script src="<?php echo $url; ?>views/assets/plugins/editable-select/jquery-editable-select.min.js"></script>
-        <script src="<?php echo $url; ?>views/assets/plugins/jqueryLoading/loading.js"></script>
-        <script type="text/javascript" src="<?php echo $url; ?>views/assets/plugins/DataTables/datatables.min.js"></script>
-        <!-- Custom scripts -->
-        <script src="<?php echo $url; ?>views/assets/js/scripts/general.js?v='<?php echo $v; ?>'"></script>
-        <script src="<?php echo $url; ?>views/assets/js/scripts/login.js?v='<?php echo $v; ?>'"></script>
-        <script src="<?php echo $url; ?>views/assets/js/scripts/registrarme.js?v='<?php echo $v; ?>'"></script>
-        <!-- Administración -->
-        <script src="<?php echo $url; ?>views/assets/js/scripts/admin_usuarios.js?v='<?php echo $v; ?>'"></script>
-        <script src="<?php echo $url; ?>views/assets/js/scripts/admin_paises.js?v='<?php echo $v; ?>'"></script>
-        <!-- Cafeterías -->
-        <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias.js?v='<?php echo $v; ?>'"></script>
-        <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias_mapa.js?v='<?php echo $v; ?>'"></script>
-        <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias_lista.js?v='<?php echo $v; ?>'"></script>
-        <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias_servicios.js?v='<?php echo $v; ?>'"></script>
-        <!-- Menu -->
-        <script src="<?php echo $url; ?>views/assets/js/scripts/menu_categorias.js?v='<?php echo $v; ?>'"></script>
-        <script src="<?php echo $url; ?>views/assets/js/scripts/menu_productos.js?v='<?php echo $v; ?>'"></script>
-        <!-- Dashboard -->
-        <script src="<?php echo $url; ?>views/assets/js/scripts/dashboard.js?v='<?php echo $v; ?>'"></script>
+    <!-- Popper.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 
-    </body>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- SweetAlert -->
+    <script src="<?php echo $url; ?>views/assets/plugins/sweetalert/dist/sweetalert.min.js"></script>
+
+    <!-- jQuery Custom Scroller CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
+    <!-- scripts -->
+    <script src="<?php echo $url; ?>views/assets/plugins/chartjs/chart.js"></script>
+    <script src="<?php echo $url; ?>views/assets/plugins/moment/min/moment.min.js"></script>
+    <script src="<?php echo $url; ?>views/assets/css/js/main.js"></script>
+    <script src="<?php echo $url; ?>views/assets/css/js/color-modes.js"></script>
+    <script src="<?php echo $url; ?>views/assets/plugins/select2/dist/js/select2.full.min.js"></script>
+    <script src="<?php echo $url; ?>views/assets/plugins/editable-select/jquery-editable-select.min.js"></script>
+    <script src="<?php echo $url; ?>views/assets/plugins/jqueryLoading/loading.js"></script>
+    <script type="text/javascript" src="<?php echo $url; ?>views/assets/plugins/DataTables/datatables.min.js"></script>
+    <!-- Custom scripts -->
+    <script src="<?php echo $url; ?>views/assets/js/scripts/general.js?v='<?php echo $v; ?>'"></script>
+    <script src="<?php echo $url; ?>views/assets/js/scripts/login.js?v='<?php echo $v; ?>'"></script>
+    <script src="<?php echo $url; ?>views/assets/js/scripts/registrarme.js?v='<?php echo $v; ?>'"></script>
+    <!-- Administración -->
+    <script src="<?php echo $url; ?>views/assets/js/scripts/admin_usuarios.js?v='<?php echo $v; ?>'"></script>
+    <script src="<?php echo $url; ?>views/assets/js/scripts/admin_paises.js?v='<?php echo $v; ?>'"></script>
+    <!-- Cafeterías -->
+    <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias.js?v='<?php echo $v; ?>'"></script>
+    <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias_mapa.js?v='<?php echo $v; ?>'"></script>
+    <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias_lista.js?v='<?php echo $v; ?>'"></script>
+    <script src="<?php echo $url; ?>views/assets/js/scripts/cafeterias_servicios.js?v='<?php echo $v; ?>'"></script>
+    <!-- Menu -->
+    <script src="<?php echo $url; ?>views/assets/js/scripts/menu_categorias.js?v='<?php echo $v; ?>'"></script>
+    <script src="<?php echo $url; ?>views/assets/js/scripts/menu_productos.js?v='<?php echo $v; ?>'"></script>
+    <!-- Dashboard -->
+    <script src="<?php echo $url; ?>views/assets/js/scripts/dashboard.js?v='<?php echo $v; ?>'"></script>
+
+</body>
+
 </html>
